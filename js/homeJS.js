053bb23507d6058972d475637d47c02e40fb8f25
Josh Agda <agda0627@hotmail.com>
2017-01-03 13:53:28 -0500

    Add files via upload

---
+++ js/homeJS.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    /*var showTerm1 = function () {
+        $("#term1Content").show();
+        $("#introContent").hide();
+    }
+
+    var showIntro = function () {
+        $("#introContent").show();
+        $("#term1Content").hide();
+
+    }
+
+    $("#term1Button").click(function () {
+        showTerm1();
+    })
+
+    $("#introButton").click(function () {
+        showIntro();
+    })*/
+})
